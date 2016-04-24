165f6d314262355e58dabc3811e2722e0523e26d
Zhuoli <zhuoliseattle@gmail.com>
2016-04-23 20:57:59 -0700

    pdf

--- SwiftSpring2016/ViewController.swift
+++ SwiftSpring2016/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var userIsInTheMiddleOfTyping = false    
+    var userIsInTheMiddleOfTyping = false
     
     @IBOutlet weak var display: UILabel!
     
